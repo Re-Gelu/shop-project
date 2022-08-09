3dbe35a1ce0471c75c16b4d99b7a054be83a3f2e
Re_Gelu <morandva@yandex.ru>
2022-08-09 15:50:42 +0300

    .gitignore

--- Shop/forms.py
+++ Shop/forms.py
@@ -24,6 +24,7 @@ class RegistrationForm(UserCreationForm):
         
         for key in self.fields.keys():
             self.fields[key].widget.attrs['class'] = 'form-control'
+            
     class Meta(UserCreationForm.Meta):
         model = User
         fields = ("email", "first_name", "last_name", "password1", "password2")
