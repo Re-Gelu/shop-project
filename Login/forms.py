f8a917765c38ea5fbd1aa873e87a86d1aed38ec8
Re_Gelu <morandva@yandex.ru>
2022-09-15 22:19:36 +0300

    Updated Login module

--- Login/forms.py
+++ Login/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, PasswordChangeForm
 
+
 class LoginForm(AuthenticationForm):
 
     def __init__(self, *args, **kwargs):
