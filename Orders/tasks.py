73ecf424b086402e6f7f799a41dfae88cdb07dd3
Re_Gelu <morandva@yandex.ru>
2022-10-21 16:28:41 +0300

    New task

--- Orders/tasks.py
+++ Orders/tasks.py
@@ -1,12 +1,15 @@
 from celery import shared_task
 from .models import Orders
+from django_celery_results.models import TaskResult
+from django.utils import timezone
+import datetime
 from .QIWI import get_QIWI_p2p
 
 @shared_task
 def payment_handler():
     result = "Waiting for new payment statuses..."
     p2p = get_QIWI_p2p()
-    if Orders.objects.filter(status=Orders.Payment_statuses.CREATED).exists() or Orders.objects.filter(status=Orders.Payment_statuses.WAITING).exists():
+    if p2p != False and (Orders.objects.filter(status=Orders.Payment_statuses.CREATED).exists() or Orders.objects.filter(status=Orders.Payment_statuses.WAITING).exists()):
         for order in Orders.objects.filter(status=Orders.Payment_statuses.CREATED) or Orders.objects.filter(status=Orders.Payment_statuses.WAITING):
             payment_status = p2p.check(order.UUID).status
             if order.status != payment_status:
@@ -21,6 +24,11 @@ def payment_handler():
         return result
     else:
         return "No orders to handle for now..."
+    
+def delete_1_week_old_payment_handler_results():
+    for order in TaskResult.objects.filter(date_done__lte=timezone.now() - datetime.timedelta(days=7)):
+        #order.delete()
+        pass
             
 @shared_task
 def debug_task():
