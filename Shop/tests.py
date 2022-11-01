c263aff29e2e54a22560181ae159ae633c6a17d5
Re_Gelu <morandva@yandex.ru>
2022-11-01 14:56:17 +0300

    Добавлены тесты

--- Shop/tests.py
+++ Shop/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.urls import reverse, resolve
 from django.contrib.auth.models import User
 from .views import *
+from .models import *
 
 class IndexViewTests(TestCase):
     
@@ -131,13 +132,15 @@ class DeliveryViewTests(TestCase):
         )
 
 class DashboardViewTests(TestCase):
+    username = 'superadmin'
+    password = 'testpass123'
 
     @classmethod
     def setUpTestData(cls):
         User.objects.create_superuser('superadmin', 'superadmin@email.com', 'testpass123')
 
     def setUp(self):
-        self.client.login(username='superadmin', password='testpass123')
+        self.client.login(username=self.username, password=self.password)
         self.url = reverse('dashboard')
         self.response = self.client.get(self.url)
         
@@ -155,14 +158,15 @@ class DashboardViewTests(TestCase):
             DashboardPageView.as_view().__name__
         )
 class db_auto_fillViewTests(TestCase):
+    username = 'superadmin'
+    password = 'testpass123'
 
     @classmethod
     def setUpTestData(cls):
-        User.objects.create_superuser(
-            'superadmin', 'superadmin@email.com', 'testpass123')
+        User.objects.create_superuser('superadmin', 'superadmin@email.com', 'testpass123')
 
     def setUp(self):
-        self.client.login(username='superadmin', password='testpass123')
+        self.client.login(username=self.username, password=self.password)
         self.db_auto_fill_response_1 = self.client.get('/db_auto_fill/3/Categories/')
         self.db_auto_fill_response_2 = self.client.get('/db_auto_fill/10/Subcategories/')
         self.db_auto_fill_response_3 = self.client.get('/db_auto_fill/50/Products/')
@@ -177,3 +181,61 @@ class db_auto_fillViewTests(TestCase):
         self.assertContains(self.db_auto_fill_response_1, 'Успешно добавлено 3 записей в таблицу Categories!')
         self.assertContains(self.db_auto_fill_response_2, 'Успешно добавлено 10 записей в таблицу Subcategories!')
         self.assertContains(self.db_auto_fill_response_3, 'Успешно добавлено 50 записей в таблицу Products!')
+        
+class ProductsModelTests(TestCase):
+    def setUp(self):
+        self.product = Products.objects.create(
+            name='test product',
+            price=200,
+            promo_price=250,
+            stock=100,
+            available=True
+        )
+    
+    def test_product_listing(self):
+        self.assertEqual(self.product.name, 'test product')
+        self.assertEqual(self.product.price, 200)
+        self.assertIsNone(self.product.promo_price)
+        self.assertEqual(self.product.stock, 100)
+        self.assertTrue(self.product.available)
+
+    def test_product_view(self):
+        response = self.client.get('/product/?id=1')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'test product')
+        self.assertTemplateUsed(response, 'product_page.html')
+        
+class CategoriesModelTests(TestCase):
+    def setUp(self):
+        self.category = Categories.objects.create(
+            name='test category'
+        )
+    
+    def test_category_listing(self):
+        self.assertEqual(self.category.name, 'test category')
+        
+    def test_category_view(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'test category')
+        self.assertTemplateUsed(response, 'index.html')
+
+
+class SubcategoriesModelTests(TestCase):
+    def setUp(self):
+        self.category = Categories.objects.create(
+            name='test category'
+        )
+        self.subcategory = Subcategories.objects.create(
+            name='test subcategory',
+            category=self.category
+        )
+        
+    def test_subcategory_listing(self):
+        self.assertEqual(self.subcategory.name, 'test subcategory')
+        
+    def test_subcategory_view(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'test subcategory')
+        self.assertTemplateUsed(response, 'index.html')
