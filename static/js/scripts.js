096222eb70992cb8abec9b7f7b8b9669217d51f3
Re_Gelu <morandva@yandex.ru>
2022-11-14 03:15:36 +0300

    JS fixes

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,57 +1,16 @@
-/* var sum = 2 + 5;
-var result = 100 < 1000 && 100 > 0;
-console.log(result);
-
-function print_hello() {
-  console.log("Hello world");
-} */
-
-class User {
-	#name
-	#age
-
-	constructor(name, age) {
-		this.#name = name
-		this.#age = age
+function handleClick(e){
+	if (e.shiftKey) {
+		console.log("screenX: " + e.screenX);
+		console.log("screenY: " + e.screenY);
+		console.log("clientX: " + e.clientX);
+		console.log("clientY: " + e.clientY);
 	}
-
-	get name() {return this.#name}
-	get age() {return this.#age}
-	set age(value) {
-		if (0 <= value <=100){
-			this.#age = value
-		}
-	}
-
-	get_main_info() {return `Name: ${this.name}, Age: ${this.age}`}
 }
 
-class Employee extends User {
-	#company
+const btn = document.getElementById("header");
+btn.addEventListener("mouseover", (e) => e.target.style.backgroundColor = "red", true);
+btn.addEventListener("mouseout", (e) => e.target.style.backgroundColor = "white", true);
+btn.addEventListener("click", handleClick, true);
 
-	constructor(name, age, company) {
-		super(name, age)
-		this.#company = company
-	}
-
-	get_main_info() {return `${super.get_main_info()}, Company: ${this.#company}`}
-}
-
-
-var TestUser = new User("One", 18)
-TestUser.age = 20
-console.log(TestUser.get_main_info())
-
-var TestEmployee = new Employee("Second", 21, "yandex")
-console.log(TestEmployee.get_main_info());
-
-const person = "tom";
-
-function check (parts, name) {
-	console.log(parts);
-	return parts[0] + name + parts[1];
-}
-
-let checkedTemplate = check`Person: ${person}`;
-console.log(checkedTemplate);
-document.body.innerHTML = checkedTemplate;
+for(var i=0; i<searchForm.elements.length;i++)
+    document.write(searchForm.elements[i].name + "<br/>");
