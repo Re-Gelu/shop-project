348a09ceac0a4acc51576c57e52adfd0e2974f4b
Re:Gelu <morandva@yandex.ru>
2022-12-19 00:06:19 +0300

    more jQuery!!!

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -7,8 +7,8 @@ $.ajaxSetup({
 })
 
 const basic_url =  window.location.protocol + '//' + window.location.host + '/'
-
-/* Event handler to add one product in cart */
+    
+/* Event handler to add one piece of product in cart */
 document.querySelectorAll('[id=cart-add-one-btn]').forEach( (element) => {
     element.onclick = () => {
         const product_id = parseInt(element.getAttribute("product-id"))
@@ -30,7 +30,7 @@ document.querySelectorAll('[id=cart-add-one-btn]').forEach( (element) => {
     };
 });
 
-/* Event handler to remove one product from cart */
+/* Event handler to remove one piece of product from cart */
 document.querySelectorAll('[id=cart-remove-one-btn]').forEach( (element) => {
     element.onclick = () => {
         const product_id = parseInt(element.getAttribute("product-id"))
@@ -39,6 +39,28 @@ document.querySelectorAll('[id=cart-remove-one-btn]').forEach( (element) => {
             "id": product_id,
             "action": false,
             "amount": 1
+        }
+
+        $.post(
+            basic_url + 'api/cart/',
+            data,
+            (data, textStatus) => {
+                console.log(textStatus, data);
+                $('body').load(document.location.href);
+            }
+        );
+    };
+});
+
+/* Event handler to remove one product position from cart */
+document.querySelectorAll('[id=cart-remove-btn]').forEach( (element) => {
+    element.onclick = () => {
+        const product_id = parseInt(element.getAttribute("product-id"))
+
+        const data = {
+            "id": product_id,
+            "action": false,
+            "amount": 100
         }
 
         $.post(
