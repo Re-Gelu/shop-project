e7263f6bba175e5da8008a7451ce31e617305897
Re:Gelu <morandva@yandex.ru>
2022-12-19 21:21:40 +0300

    Scripts DRY fix

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -4,78 +4,71 @@ $.ajaxSetup({
     "headers": {
         "X-CSRFToken": csrf_token
     }
-})
-
-const basic_url =  window.location.protocol + '//' + window.location.host + '/'
-
-
-/* MAIN CART BUTTONS EVENT HANDLERS */
-
-    
-/* Event handler to add one piece of product in cart */
-document.querySelectorAll('[id=cart-add-one-btn]').forEach( (element) => {
-    element.onclick = () => {
-        const product_id = parseInt(element.getAttribute("product-id"))
-
-        const data = {
-            "id": product_id,
-            "action": true,
-            "amount": 1,
-        }
-
-        $.post(
-            basic_url + 'api/cart/',
-            data,
-            (data, textStatus) => {
-                console.log(textStatus, data);
-                $('body').load(document.location.href);
-            }
-        );
-    };
 });
+
+const basic_url =  window.location.protocol + '//' + window.location.host + '/';
+
+
+/* CART BUTTONS EVENT HANDLERS */
+
+
+const event = (data, textStatus) => {
+    console.log(textStatus, data);
+    $('#dashboard-cart').load(basic_url + 'api/dashboard_cart/');
+    $('#cart-offcanvas-body').load(basic_url + 'api/header_offcanvas_body/');
+};
+
+const cart_event_handler = (ids, data) => {
+
+    document.querySelectorAll(ids).forEach( (element) => {
+        element.onclick = () => {
+            $.post(
+                basic_url + 'api/cart/',
+                $.extend(
+                    {"id": parseInt(element.getAttribute("product-id"))},
+                    data
+                ),
+                event
+            );
+        };
+    });
+
+};
+
+/* Event handler to add one piece of product in cart */
+cart_event_handler(
+    ids='\
+        #cart-add-one-btn, \
+        #cart-dashboard-add-one-btn, \
+        #cart-offcanvas-add-one-btn',
+    data={
+        "action": true,
+        "amount": 1,
+    }
+);
 
 /* Event handler to remove one piece of product from cart */
-document.querySelectorAll('[id=cart-remove-one-btn]').forEach( (element) => {
-    element.onclick = () => {
-        const product_id = parseInt(element.getAttribute("product-id"))
-
-        const data = {
-            "id": product_id,
-            "action": false,
-            "amount": 1
-        }
-
-        $.post(
-            basic_url + 'api/cart/',
-            data,
-            (data, textStatus) => {
-                console.log(textStatus, data);
-                $('body').load(document.location.href);
-            }
-        );
-    };
-});
+cart_event_handler(
+    ids='\
+        #cart-remove-one-btn, \
+        #cart-dashboard-remove-one-btn, \
+        #cart-offcanvas-remove-one-btn',
+    data={
+        "action": false,
+        "amount": 1
+    }
+);
 
 /* Event handler to remove one product position from cart */
-document.querySelectorAll('[id=cart-remove-btn]').forEach( (element) => {
-    element.onclick = () => {
-        const product_id = parseInt(element.getAttribute("product-id"))
-
-        const data = {
-            "id": product_id,
-            "action": false,
-            "amount": 100
-        }
-
-        $.post(
-            basic_url + 'api/cart/',
-            data,
-            (data, textStatus) => {
-                console.log(textStatus, data);
-                $('body').load(document.location.href);
-            }
-        );
-    };
-});
+cart_event_handler(
+    ids='\
+        #cart-remove-btn, \
+        #cart-dashboard-remove-btn, \
+        #cart-offcanvas-remove-btn',
+    data={
+        "action": false,
+        "amount": 100
+    }
+);
 
 });
