81f5210103f0cf522de5ab02531b3a27cc4291d4
Re:Gelu <morandva@yandex.ru>
2023-01-24 03:38:53 +0300

    Small code refactoring

--- Site/settings.py
+++ Site/settings.py
@@ -528,9 +528,4 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
 ]
 
-CSRF_TRUSTED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000"
-]
-
 CORS_ALLOW_CREDENTIALS = True
