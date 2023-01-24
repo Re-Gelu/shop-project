1a96ac0c20838bb466b6c15035af26369aeb725a
Re:Gelu <morandva@yandex.ru>
2023-01-24 03:21:08 +0300

    Small code refactoring

--- Site/settings.py
+++ Site/settings.py
@@ -91,7 +91,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.cache.CacheMiddleware',
+    #'django.middleware.cache.CacheMiddleware',
     
     'django.contrib.sessions.middleware.SessionMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
