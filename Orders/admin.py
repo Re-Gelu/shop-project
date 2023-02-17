3fcca3333dd89842612c90b98b2c188aff790853
Re:Gelu <75813517+Re-Gelu@users.noreply.github.com>
2023-02-17 19:02:41 +0300

    Delete Orders directory

--- Orders/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-
-@admin.register(orders)
-class OrdersAdmin(admin.ModelAdmin):
-    list_display = ("UUID", "created", "status")
-    list_filter = ("created", "status")
-    search_fields = ("UUID", "adress", "status")
--- Orders/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class OrdersConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'orders'
-    verbose_name = 'Заказы'
